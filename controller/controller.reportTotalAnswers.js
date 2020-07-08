726b6798aa689de0b135e7983e4f053939d5405f
Hein Min Htet <57899239+Hein21-bot@users.noreply.github.com>
2020-07-08 17:22:56 +0630

    BMSandArea

--- controller/controller.reportTotalAnswers.js
+++ controller/controller.reportTotalAnswers.js
@@ -93,29 +93,27 @@ const userLevelMenuAnswer = (req, res) => {
 const typeAndArea = (req, res) => {
   reportTotalAnswersService.typeAndArea()
     .then(data => {
-      // let surveySections = Object.keys(groupArray(data, "option_choice_name")).map((v, k) => {
-      //   return groupArray(data, "option_choice_name")[v];
-      // }).map((v1, k1) => {
-      //   return {
-      //     option_choice_name: v1[0].option_choice_name,
-      //     categories: Object.keys(groupArray(v1, "building_type")).map((v2, k2) => {
-      //       return groupArray(v1, "building_type")[v2]
-      //     }).map((v3, k3) => {
-      //       return {
-      //         building_type: v3[0].building_type,
-      //         optionCount: v3[0].optionCount
-      //       }
-      //     }).map((categories => {
-      //       let rObj = {}
-      //       rObj[categories.building_type] = categories.optionCount
-      //       return rObj
-      //     })).reduce(function(result, current) {
-      //       return Object.assign(result, current);
-      //     }, {})
-      //   }
-      // })
+      let surveySections = Object.keys(groupArray(data, "option_choice_name")).map((v, k) => {
+        return groupArray(data, "option_choice_name")[v];
+      }).map((v1, k1) => {
+        return {
+          option_choice_name: v1[0].option_choice_name,
+          categories: Object.keys(groupArray(v1, "building_type")).map((v2, k2) => {
+            return groupArray(v1, "building_type")[v2]
+          }).map((v3, k3) => {
+            return {
+              building_type: v3[0].building_type,
+              optionCount: v3[0].optionCount
+            }
+          }).map((categories => {
+            let rObj = {}
+            rObj[categories.building_type] = categories.optionCount
+            return rObj
+          })).reduce(((r, c) => Object.assign(r, c)), {})
+        }
+      })
 
-      res.json(response({ success: true, payload: data }));
+      res.json(response({ success: true, payload: surveySections }));
     })
     .catch((err) =>
       res.json(response({ success: false, message: err.toString() }))
@@ -135,14 +133,18 @@ const typeAndBMS = (req, res) => {
             return {
               option_choice_name: v3[0].option_choice_name
             }
-          }),
+          }).map((categories => {
+            return categories.option_choice_name
+          })),
           data: Object.keys(groupArray(v1, "option_choice_name")).map((v4, k4) => {
             return groupArray(v1, "option_choice_name")[v4];
           }).map((v5, k5) => {
             return {
               option_count: v5[0].option_count,
             };
-          }),
+          }).map((categories => {
+            return categories.option_count
+          }))
         }
       })
 
@@ -152,5 +154,6 @@ const typeAndBMS = (req, res) => {
       res.json(response({ success: false, message: err.toString() }))
     );
 }
+
 
 module.exports = { userLevelAnswer, userLevelMenuAnswer, typeAndArea, typeAndBMS };
